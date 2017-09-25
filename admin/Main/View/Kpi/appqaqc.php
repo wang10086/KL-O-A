726134a8aec64fd19cb4f17e5101fc61aa2290a7
ChengLi <chengli@btte.net>
2017-09-25 11:04:12 +0800

    gx

--- admin/Main/View/Kpi/appqaqc.php
+++ admin/Main/View/Kpi/appqaqc.php
@@ -71,7 +71,7 @@
                                         
                                         
                                         <div class="form-group box-float-12">
-                                            <label>不合格事实陈序及违反规定条款</label> 
+                                            <label>不合格事实陈述及违反规定条款</label> 
                                             <textarea class="form-control" style="height:90px;" name="info[chen]">{$row.chen}</textarea>
                                         </div>
                                         
