6640e24376ad81624f9f68fcb182722f732c18b7
ChengLi <chengli@btte.net>
2018-01-12 12:05:11 +0800

    gx

--- admin/Main/Controller/ExportController.class.php
+++ admin/Main/Controller/ExportController.class.php
@@ -298,27 +298,29 @@ class ExportController extends BaseController {
 		
 		$where = array();
 		$where['b.audit'] = 1;
+		$where['l.req_type']	= 801;
 		if($st && $et){
-			$where['b.create_time'] = array('between',array(strtotime($st),strtotime($et)));	
+			$where['l.audit_time'] = array('between',array(strtotime($st),strtotime($et)));	
 			$this->onmoon    = $st.'至'.$et.'结算报表';
 		}else if($st){
-			$where['b.create_time'] = array('gt',strtotime($st));		
+			$where['l.audit_time'] = array('gt',strtotime($st));		
 			$this->onmoon    = $st.'之后结算报表';
 		}else if($et){
-			$where['b.create_time'] = array('lt',strtotime($et));	
+			$where['l.audit_time'] = array('lt',strtotime($et));	
 			$this->onmoon    = $et.'之前结算报表';	
 		}else if($yue){
-			$where['b.create_time'] = array('between',array($moon['start'],$moon['end']));
+			$where['l.audit_time'] = array('between',array($moon['start'],$moon['end']));
 			$this->onmoon    = date('Y年m月份',$moon['start']).'结算报表';
 		}else{
 			$this->onmoon    = '结算报表';	
 		}
-		if($xs)   $where['o.create_user_name'] = array('like','%'.$xs.'%');
-		if($dept) $where['o.create_user'] = array('in',Rolerelation($dept,1));
+		if($xs)   $where['o.create_user_name']	= array('like','%'.$xs.'%');
+		if($dept) $where['a.group_role']		= array('like','%['.$dept.']%');
+		
 		
 		
 		$expdata  = array();
-		$datalist = $db->table('__OP_SETTLEMENT__ as b')->field('b.*,o.project,o.group_id,o.number,o.customer,o.create_user_name,o.destination,o.days,o.remark')->join('__OP__ as o on b.op_id = o.op_id','LEFT')->where($where)->order('b.create_time DESC')->select();
+		$datalist = $db->table('__OP_SETTLEMENT__ as b')->field('b.*,o.project,o.group_id,o.number,o.customer,o.create_user_name,o.destination,o.days,o.remark,l.audit_time')->join('__OP__ as o on b.op_id = o.op_id','LEFT')->join('__AUDIT_LOG__ as l on l.req_id = b.id','LEFT')->join('__ACCOUNT__ as a on a.id = o.create_user','LEFT')->where($where)->limit($page->firstRow . ',' . $page->listRows)->order('l.audit_time DESC')->select();
 		foreach($datalist as $k=>$v){
 			
 			$title    = array();
