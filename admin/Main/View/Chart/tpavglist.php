3c1db4e23c592d73288aefd1b16f7c34c7228d2b
ChengLi <chengli@btte.net>
2018-01-15 11:51:00 +0800

    gx

--- admin/Main/View/Chart/tpavglist.php
+++ admin/Main/View/Chart/tpavglist.php
@@ -44,7 +44,7 @@
                                         <foreach name="lists" item="row" key="k">                      
                                             <tr>
                                                 <td class="orderNo"></td>
-                                                <td>{$row.rolename}</td>
+                                                <td><a href="{:U('Chart/finance',array('dept'=>$row['rid'],'st'=>'2018-01-01'))}">{$row.rolename}</a></td>
                                                 <td>{$row.num}</td>
                                                 <td>{$row.rjzsr}</td>
                                                 <td>{$row.rjzml}</td>
