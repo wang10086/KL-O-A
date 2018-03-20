9d8926a9bfc088cb9cf0a6e33c9334a843129f97
root <1021056947@qq.com>
2018-03-20 16:01:40 +0800

    aa

--- admin/Common/Conf/db_config.php
+++ admin/Common/Conf/db_config.php
@@ -8,6 +8,8 @@ return array(
     'DB_NAME'               =>  'xuequoa_test',			// 数据库名
     'DB_USER'               =>  'kl_o2o',			// 用户名
     'DB_PWD'                =>  'kelvo2o',			// 密码
+    //'DB_USER'               =>  'root',			// 用户名
+    //'DB_PWD'                =>  '123456',			// 密码
     'DB_PORT'               =>  '3306',				// 端口
     'DB_PREFIX'             =>  'oa_',
 	
