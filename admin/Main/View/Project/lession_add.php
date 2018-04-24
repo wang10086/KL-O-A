b68ab3bbc1f26c480896adf88c4ee7a703b99c2e
wang10086 <1021056947@qq.com>
2018-04-24 23:16:23 +0800

    test

--- admin/Main/View/Project/lession_add.php
+++ admin/Main/View/Project/lession_add.php
@@ -82,6 +82,47 @@
                                     
                                 </div><!-- /.box-body -->
                             </div><!-- /.box -->
+
+                            <!--<div class="box box-warning">
+                                <div class="box-header">
+                                    <h3 class="box-title">上传附件</h3>
+
+                                </div>
+                                <div class="box-body">
+                                    <div class="content">
+                                        <div class="form-group col-md-12">
+                                            <table id="flist" class="table" style="margin-top:10px;">
+                                                <tr>
+                                                    <th align="left" width="45%">文件名称</th>
+                                                    <th align="left" width="10%">大小</th>
+                                                    <th align="left" width="30%">上传进度</th>
+                                                    <th align="left" width="15%">操作</th>
+                                                </tr>
+                                                <foreach name="atts" item="v">
+                                                    <tr id="aid_{$v.id}" valign="middle">
+                                                        <td><input type="text" name="newname[{$v.id}]" value="{$v.filename}" class="form-control"  /></td>
+                                                        <td>{:fsize($v['filesize'])}</td>
+                                                        <td>
+                                                            <div class="progress sm">
+                                                                <div class="progress-bar progress-bar-aqua" rel="aid_{$v.id}"  role="progressbar" style="width: 100%;"  aria-valuemin="0" aria-valuemax="100">
+                                                                </div>
+                                                            </div>
+                                                        </td>
+                                                        <td><a class="btn btn-danger btn-xs " href="javascript:;" onclick="removeThisFile('aid_{$v.id}');"><i class="fa fa-times"></i>删除</a>&nbsp;&nbsp;&nbsp;&nbsp; <a class="btn btn-success btn-xs " href="{$v.filepath}" onclick=""><i class="fa fa-download"></i>下载</a></td>
+                                                    </tr>
+                                                </foreach>
+                                            </table>
+
+                                            <a href="javascript:;" id="pickupfile" class="btn btn-success btn-sm" style="margin-top:15px;"><i class="fa fa-upload"></i> 上传附件</a>
+                                            <div id="container" style="display:none;">
+                                                <foreach name="atts" item="v">
+                                                    <input type="hidden" rel="aid_{$v.id}" name="resfiles[]" value="{$v.id}" />
+                                                </foreach>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>-->
                             
                            
                             <div style="width:100%; text-align:center;">
@@ -158,4 +199,92 @@
         })
 
     }
+</script>
+
+<script type="text/javascript">
+
+    $(document).ready(function(e) {
+        var uploader = new plupload.Uploader({
+            runtimes : 'html5,flash,silverlight,html4',
+            browse_button : 'pickupfile', // you can pass in id...
+            container: document.getElementById('container'), // ... or DOM Element itself
+            url : 'index.php?m=Main&c=File&a=upload_file',
+            flash_swf_url : '__HTML__/comm/plupload/Moxie.swf',
+            silverlight_xap_url : '__HTML__/comm/plupload/Moxie.xap',
+            multiple_queues:false,
+            multipart_params: {
+                catid: 1
+            },
+
+            filters : {
+                max_file_size : '10mb',
+                mime_types: [
+                    {title : "Files", extensions : "jpg,jpeg,png,zip,rar,7z,doc,docx,ppt,pptx,xls,xlsx,txt"}
+                ]
+            },
+
+            init: {
+                PostInit: function() {
+                    //$('div.moxie-shim').width(104).height(67);
+                },
+
+                FilesAdded: function(up, files) {
+                    plupload.each(files, function(file) {
+                        var time = new Date();
+                        var month = time.getMonth() +1;
+                        if (month < 10) month = "0" + month;
+
+                        var t = time.getFullYear()+ "/"+ month + "/" + time.getDate()+ " "+time.getHours()+ ":"+ time.getMinutes() + ":" +time.getSeconds();
+                        $('#flist').append(
+                            '<tr id="' + file.id + '"  valign="middle"><td>'
+                            + '<input type="text" name="nm_' + file.id + '" value="'+ file.name +'" class="form-control" />'
+                            + '</td> <td width="10%">' + plupload.formatSize(file.size) +''
+                            + '</td> <td width="30%">'
+                            + '<div class="progress sm"> '
+                            + '<div class="progress-bar progress-bar-aqua" rel="'+ file.id +'"  role="progressbar"  aria-valuemin="0" aria-valuemax="100">'
+                            + '</div></div></td>'
+                            + '<td width="15%"><a class="btn btn-danger btn-xs " href="javascript:;" onclick="removeThisFile(\''+ file.id +'\');"><i class="fa fa-times"></i>删除</a></td></tr>'
+                        );
+
+                    });
+
+                    uploader.start();
+
+                },
+
+                FileUploaded: function(up, file, res) {
+                    var rs = eval('(' + res.response +')');
+                    if (rs.rs ==  'ok') {
+                        $('div[rel=' + file.id + ']').css('width', '100%');
+                        $('#container').append('<input type="hidden" rel="'+file.id+'" name="resfiles[]" value="' + rs.aid + '" />');
+                        $('input[name=nm_' + file.id +']').prop('name', 'newname['+rs.aid+']');
+                    } else {
+                        alert('上传文件失败，请重试');
+                    }
+
+                },
+
+                UploadProgress: function(up, file) {
+                    $('div[rel=' + file.id + ']').css('width', file.percent + '%');
+                },
+
+                Error: function(up, err) {
+                    alert(err.code + ": " + err.message);
+                }
+            }
+        });
+
+        uploader.init();
+
+        closebtns();
+    });
+
+    function removeThisFile(fid) {
+        if (confirm('确定要删除此附件吗？')) {
+            $('#' + fid).empty().remove();
+            $('input[rel=' + fid +']').remove();
+        }
+    }
+
+
 </script>
