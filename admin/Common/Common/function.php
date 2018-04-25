ec1dc0172f4a7701e83c35b03055efe8a25d7c8f
ChengLi <chengli@btte.net>
2018-04-25 14:27:35 +0800

    gx

--- admin/Common/Common/function.php
+++ admin/Common/Common/function.php
@@ -1896,12 +1896,12 @@ function updatekpi($month,$user){
 					$where['b.audit_status']		= 1;
 					$where['o.create_user']			= $user;
 					$where['l.req_type']			= 801;
-					$end_time 						= $v['end_date']+7*24*3600;
-					/*$where['l.audit_time']			= array('between',array($v['start_date'],$v['end_date']));*/
-					$where['l.audit_time']			= array('between',array($v['start_date'],$end_time));
-					$xiangmu = M()->table('__OP_SETTLEMENT__ as b')->field('b.maoli')->join('__OP__ as o on b.op_id = o.op_id','LEFT')->join('__AUDIT_LOG__ as l on l.req_id = b.id','LEFT')->where($where)->count();
+					$end_time 						= $v['end_date']-(7*86400);
+					$where['l.audit_time']			= array('between',array($v['start_date'],$v['end_date']));
+					$xiangmu = M()->table('__OP_SETTLEMENT__ as b')->field('b.maoli')->join('__OP__ as o on b.op_id = o.op_id','LEFT')->join('__AUDIT_LOG__ as l on l.req_id = b.id','LEFT')->where($where)->count();//getField('b.op_id',true);
+					
 					$hetong  = M()->table('__CONTRACT__ as c')->field('c.*')->join('__OP_SETTLEMENT__ as b on b.op_id = c.op_id','LEFT')->join('__OP__ as o on o.op_id = c.op_id','LEFT')->join('__AUDIT_LOG__ as l on l.req_id = b.id','LEFT')->where($where)->count();
-					$complete = round(($hetong / $xiangmu)*100,2).'%';
+					$complete = $xiangmu ? round(($hetong / $xiangmu)*100,2).'%' : 100 .'%';
 				}
 				
 				
