6a8668cbf41895813f894311fb77137a58e4c9b5
root <1021056947@qq.com>
2018-05-02 10:39:28 +0800

    100M

--- admin/Main/View/Project/lession_add.php
+++ admin/Main/View/Project/lession_add.php
@@ -217,7 +217,7 @@
             },
 
             filters : {
-                max_file_size : '60mb',
+                max_file_size : '100mb',
                 mime_types: [
                     {title : "Files", extensions : "jpg,jpeg,png,zip,rar,7z,doc,docx,ppt,pptx,xls,xlsx,txt"}
                 ]
