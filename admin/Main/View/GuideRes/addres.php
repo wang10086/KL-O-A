339be04e4eeb68bd4db9b944efede2a3f880c975
root <1021056947@qq.com>
2018-05-08 18:31:41 +0800

    aa

--- admin/Main/View/GuideRes/addres.php
+++ admin/Main/View/GuideRes/addres.php
@@ -115,8 +115,54 @@
                                             <option value="1" <?php if ($row && $row['type'] == 1) echo ' selected'; ?>>专职</option>
                                         </select>
                                     </div>
-                                    
-                                    
+
+                                    <!-------------------------------------------start----------------------------------------------------->
+                                    <!--<div class="content" style="padding-top:0px;">
+                                            <div id="pretium">
+                                                <div class="userlist">
+                                                    <div class="unitbox">价格名称</div>
+                                                    <div class="unitbox">销售价</div>
+                                                    <div class="unitbox">同行价</div>
+                                                    <div class="unitbox">包含成人人数</div>
+                                                    <div class="unitbox">包含儿童人数</div>
+                                                    <div class="unitbox">备注</div>
+                                                </div>
+                                                <?php /*if($pretium){ */?>
+                                                    <foreach name="pretium" key="k" item="v">
+                                                        <div class="userlist" id="pretium_id_{$v.id}">
+                                                            <span class="title"><?php /*echo $k+1; */?></span>
+                                                            <input type="hidden" name="resid[888{$v.id}][id]" value="{$v.id}" >
+                                                            <input type="text" class="form-control" name="pretium[888{$v.id}][pretium]" value="{$v.pretium}">
+                                                            <input type="text" class="form-control" name="pretium[888{$v.id}][sale_cost]" value="{$v.sale_cost}">
+                                                            <input type="text" class="form-control" name="pretium[888{$v.id}][peer_cost]" value="{$v.peer_cost}">
+                                                            <input type="text" class="form-control" name="pretium[888{$v.id}][adult]" value="{$v.adult}">
+                                                            <input type="text" class="form-control" name="pretium[888{$v.id}][children]" value="{$v.children}">
+                                                            <input type="text" class="form-control" name="pretium[888{$v.id}][remark]" value="{$v.remark}">
+                                                            <a href="javascript:;" class="btn btn-danger btn-flat" onclick="delbox('pretium_id_{$v.id}')">删除</a>
+                                                        </div>
+                                                    </foreach>
+                                                <?php /*}else{ */?>
+                                                    <div class="userlist" id="pretium_id">
+                                                        <span class="title">1</span>
+                                                        <input type="text" class="form-control" name="pretium[888{$v.id}][pretium]" value="基础价">
+                                                        <input type="text" class="form-control" name="pretium[888{$v.id}][sale_cost]" value="">
+                                                        <input type="text" class="form-control" name="pretium[888{$v.id}][peer_cost]" value="">
+                                                        <input type="text" class="form-control" name="pretium[888{$v.id}][adult]" value="">
+                                                        <input type="text" class="form-control" name="pretium[888{$v.id}][children]" value="">
+                                                        <input type="text" class="form-control" name="pretium[888{$v.id}][remark]" value="">
+                                                        <a href="javascript:;" class="btn btn-danger btn-flat" onclick="delbox('pretium_id')">删除</a>
+                                                    </div>
+                                                <?php /*} */?>
+                                            </div>
+                                            <div id="pretium_val">1</div>
+                                            <div class="form-group col-md-12" id="useraddbtns">
+                                                <a href="javascript:;" class="btn btn-success btn-sm" onClick="add_pretium()"><i class="fa fa-fw fa-plus"></i> 新增价格信息</a>
+                                                <a href="javascript:;" class="btn btn-info btn-sm" onClick="javascript:save('save_info_price','<?php echo U('Op/public_save_price'); ?>',{$op.op_id});">保存</a>
+                                            </div>
+                                            <div class="form-group">&nbsp;</div>
+                                        </div>-->
+                                    <!---------------------------------------------end----------------------------------------------------->
+
                                     
                                     <div class="form-group col-md-12">
                                         <label>经历</label>
@@ -147,3 +193,35 @@
 
 
 <include file="Index:footer2" />
+
+<script>
+    //新增价格政策
+    function add_pretium(){
+        var i = parseInt($('#pretium_val').text())+1;
+
+        var html = '<div class="userlist" id="pretium_'+i+'"><span class="title"></span><input type="text" class="form-control" name="pretium['+i+'][pretium]"><input type="text"  class="form-control" name="pretium['+i+'][sale_cost]"><input type="text" class="form-control" name="pretium['+i+'][peer_cost]"><input type="number" class="form-control" name="pretium['+i+'][adult]"><input type="number" class="form-control" name="pretium['+i+'][children]"><input type="text" class="form-control" name="pretium['+i+'][remark]"><a href="javascript:;" class="btn btn-danger btn-flat" onclick="delbox(\'pretium_'+i+'\')">删除</a></div>';
+        $('#pretium').append(html);
+        $('#pretium_val').html(i);
+        orderno();
+    }
+
+    //移除
+    function delbox(obj){
+        $('#'+obj).remove();
+        orderno();
+    }
+
+    //编号
+    function orderno(){
+        $('#mingdan').find('.title').each(function(index, element) {
+            $(this).text(parseInt(index)+1);
+        });
+        $('#pretium').find('.title').each(function(index, element) {
+            $(this).text(parseInt(index)+1);
+        });
+        $('#costacc').find('.title').each(function(index, element) {
+            $(this).text(parseInt(index)+1);
+        });
+    }
+
+</script>
