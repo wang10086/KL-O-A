76cc9e4e27b507591b5fe7e3d75d8a838f55eed9
root <1021056947@qq.com>
2018-06-06 10:11:54 +0800

    aa

--- admin/Main/View/Op/op_res_guide.php
+++ admin/Main/View/Op/op_res_guide.php
@@ -6,35 +6,35 @@
         <thead>
             <tr>
                 <th width="">姓名</th>
-                <th width="100">联系方式</th>
-                <th width="130">职务类型</th>
-                <th width="120">职能类型</th>
-                <th width="100">性别</th>
-                <th width="100">费用</th>
-                <th width="100">数量</th>
-                <th width="100">合计</th>
-                <th width="160">备注</th>
+                <!--<th width="100">联系方式</th>-->
+                <th width="20%">职务类型</th>
+                <th width="15%">职能类型</th>
+                <th width="10%">性别</th>
+                <!--<th width="10%">费用</th>
+                <th width="10%">数量</th>-->
+                <th width="10%">合计</th>
+                <th width="20%">备注</th>
             </tr>
         </thead>
         <tbody>
             <foreach name="guide" item="v">
             <tr class="expense" >
                 <td><a href="javascript:;" onClick="open_guide({$v.guide_id},'{$v.name}')">{$v.name}</a> <i class="fa  fa-calendar" style="color:#3CF; margin-left:8px; cursor:pointer;" onClick="course({$v.guide_id},{$op.op_id})"></i></td>
-                <td>{$v.tel}</td>
+                <!--<td>{$v.tel}</td>-->
                 <td>{$v.kind}</td>
                 <td>{$v.gpk_name}</td>
                 <td>{$v.sex}</td>
-                <td>&yen;{$v.cost}</td>
-                <td>{$v.amount}</td>
+                <!--<td>&yen;{$v.cost}</td>
+                <td>{$v.amount}</td>-->
                 <!--<td class="total">&yen;{$v.total}</td>-->
                 <td class="total">&yen;<?php echo $v['cost']*$v['amount']; ?></td>
                 <td>{$v.remark}</td>
             </tr>
             </foreach>
             <tr>
+                <!--<td></td>
                 <td></td>
-                <td></td>
-                <td></td>
+                <td></td>-->
                 <td></td>
                 <td></td>
                 <td></td>
