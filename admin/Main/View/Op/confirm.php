a9bd0c6a5a754b7b0cd131e9a6ffd1efa14a5ab5
wang10086 <1021056947@qq.com>
2018-07-08 20:51:43 +0800

    aa

--- admin/Main/View/Op/confirm.php
+++ admin/Main/View/Op/confirm.php
@@ -305,10 +305,10 @@
         var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
         Y = date.getFullYear() + '-';
         M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-        D = date.getDate() + ' ';
-        h = date.getHours() + ':';
-        m = date.getMinutes() + ':';
-        s = date.getSeconds();
+        D = (date.getDate() < 10 ? '0'+(date.getDate()) : date.getDate()) + ' ';
+        h = (date.getHours() < 10 ? '0'+(date.getHours()) : date.getHours()) + ':';
+        m = (date.getMinutes() < 10 ? '0'+(date.getMinutes()) : date.getMinutes()) + ':';
+        s = (date.getSeconds() < 10 ? '0'+(date.getSeconds()) : date.getSeconds());
         //return Y+M+D+h+m+s;
         return Y+M+D;
     }
@@ -317,10 +317,10 @@
         var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
         Y = date.getFullYear() + '-';
         M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-        D = date.getDate() + ' ';
-        h = date.getHours() + ':';
-        m = date.getMinutes() + ':';
-        s = date.getSeconds();
+        D = (date.getDate() < 10 ? '0'+(date.getDate()) : date.getDate()) + ' ';
+        h = (date.getHours() < 10 ? '0'+(date.getHours()) : date.getHours()) + ':';
+        m = (date.getMinutes() < 10 ? '0'+(date.getMinutes()) : date.getMinutes()) + ':';
+        s = (date.getSeconds() < 10 ? '0'+(date.getSeconds()) : date.getSeconds());
         //return Y+M+D+h+m+s;
         return h+m+s;
     }
