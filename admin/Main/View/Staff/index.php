f989e7925243e73bd6f72386eaeba10d827472ae
wang10086 <1021056947@qq.com>
2018-08-15 22:52:31 +0800

    aa

--- admin/Main/View/Staff/index.php
+++ admin/Main/View/Staff/index.php
@@ -1,16 +1,16 @@
 <include file="header" />
 <style>
-    .staff{  width: 100%;  height:auto !important;  margin: 0;  background-color:#dceffe;  float:left; clear:both; }
-    .staff-con{ width:1200px; margin:0 auto; }
+    .staff{  width: 100%;  /*height:auto !important;*/ height: 100%;  margin: 0;  background-color:#dceffe;  float:left; clear:both; }
+    .staff-con{ width:1200px; margin:0 auto; overflow: hidden;  padding: 20px;}
     .mt20{margin-top: 20px;}
-    .actlbox{ width:895px; height:auto !important; float:left; clear:both; padding-bottom:40px; overflow:hidden;}
+    .actlbox{ width:870px; height:auto !important; float:left; clear:both; padding-bottom:40px; overflow:hidden;}
     .actlbox h1{  width:100%; float:left; clear:both; text-align:center; color:#333333; font-weight: 400;font-style: normal;font-size: 20px;}
     .actlbox .newscon{ width:100%; height:auto !important; float:left; clear:both; color:#333333;}
     .actlbox .newscon p{ font-size:14px; line-height:24px; margin:0; padding:0; padding-bottom:20px; text-indent:28px; text-align:justify;}
     .actlbox .morepic{ width:915px; float:left; clear:both;}
     .actlbox .morepic img{ width:282px; height:180px; margin-right:20px; margin-top:20px; display: inline-block;}
     .actrbox{ width:285px; height:auto !important; float:right; clear:right; overflow:hidden;}
-    .actrbox .unrel{ width:285px; min-height:300px; float:left; clear:both; background:#eeeeee;}
+    .actrbox .unrel{ width:265px; min-height:300px; float:left; clear:both; background:#eeeeee;}
     .actrbox .unrel .reltit{ width:255px; height:40px; float:left; margin:0 15px; border-bottom:1px solid #cccccc;}
     .actrbox .unrel .reltit h2{ line-height:40px; font-size:16px; color:#333333; margin: auto;}
     .cont ul{margin-top: 48px;}
@@ -18,8 +18,9 @@
 </style>
 <div class="staff">
     <div class="staff-con mt20" style="background-color:  #eeeeee;">
+        <div style="background-color:  #eeeeee;"></div>
         <div class="actlbox">
-            <div style="height: 150px; border: solid 1px red;">
+            <div style="height: 150px; border: solid 1px red; ">
                 aaa
             </div>
             <div style="height: 150px; border: solid 1px red;" class="mt10">
