7370bb697d83d1ccc5cab39aec5617719a9b8eb3
root <1021056947@qq.com>
2018-08-29 16:05:20 +0800

    aa

--- admin/Main/Controller/BaseController.class.php
+++ admin/Main/Controller/BaseController.class.php
@@ -191,211 +191,225 @@ class BaseController extends Controller {
 	    return M('audit_log')->add($data);
 	    
 	}
-	
-	protected final function do_audit($audit_id, $audit_reason = '', $dst_status = P::AUDIT_STATUS_PASS, $audit_param = '') 
-	{
-	    $id   = intval($audit_id);
-	    $row  = M('audit_log')->find($id);
-	    
-	    if (!$row) {
-	        $this->msg = "非法操作";
-	        $this->display('audit_ok');
-	        die();
-	    } 
-	    
-	    if ($row['dst_status'] != P::AUDIT_STATUS_NOT_AUDIT ) {
-	       // return P::ERROR;
-	    }
-	    $data = array();
-	    
-	    $data['audit_uid']   = session('userid');
-	    $data['audit_uname'] = session('nickname');
-	    $data['audit_reason']= $audit_reason;
-	    $data['audit_time']  = time();
-	    $data['dst_status']  = $dst_status;
-	    $data['audit_param'] = $audit_param;
-	    
-	    M()->table('__' . strtoupper($row['req_table']) . '__')
-	       ->where('id='.$row['req_id'])->setField('audit_status', $dst_status);
-	    
-	    // 物资入库处理
-	    if ($row['req_type'] == P::REQ_TYPE_GOODS_IN  && $dst_status == P::AUDIT_STATUS_PASS) {
-	       
-		    $dstdata = M()->table('__' . strtoupper($row['req_table']) . '__')->where('id='.$row['req_id'])->find();
-			$matelist = M('material_into')->where(array('batch_id'=>$dstdata['id']))->select();
-			foreach($matelist as $v){
-				$at = M('material_into')->data(array('audit_status'=>$dst_status))->where(array('id'=>$v['id']))->save();
-				if($at){
-					 if($dst_status == P::AUDIT_STATUS_PASS ){
-						 $wzinfo = M('material')->find($v['material_id']);
-						 if($v['type']){
-							  M('material')->where(array('id'=>$v['material_id']))->setField(array('stock'=>$wzinfo['stock']+$v['amount']));
-						 }else{
-							  M('material')->where(array('id'=>$v['material_id']))->setField(array('price'=>$v['unit_price'], 'stock'=>$wzinfo['stock']+$v['amount']));
-						 }
-						 
-						 if($v['op_id']){
-							 $shuliang = M('op_material')->where(array('op_id'=>$v['op_id'],'material'=>$v['material']))->find();
-							 M('op_material')->where(array('op_id'=>$v['op_id'],'material'=>$v['material']))->data(array('returnsum'=>($shuliang['returnsum']+$v['amount'])))->save();
-						 }
-					 
-					 }
-				}
-			}
 
-	    }
-	    
-	    // 物资出库处理
-	    if ($row['req_type'] == P::REQ_TYPE_GOODS_OUT) {
-			$dstdata = M()->table('__' . strtoupper($row['req_table']) . '__')->where('id='.$row['req_id'])->find();
-			$matelist = M('material_out')->where(array('batch_id'=>$dstdata['id']))->select();
-			foreach($matelist as $v){
-				$at = M('material_out')->data(array('audit_status'=>$dst_status))->where(array('id'=>$v['id']))->save();
-				if($at){
-					 if($dst_status == P::AUDIT_STATUS_PASS ){
-						 M('material')->where(array('id'=>$v['material_id']))->setDec('stock', $v['amount']);
-					 }
-					 if($v['op_id'] && $dst_status == P::AUDIT_STATUS_PASS){
-						 $shuliang = M('op_material')->where(array('op_id'=>$v['op_id'],'material'=>$v['material']))->find();
-						 M('op_material')->where(array('op_id'=>$v['op_id'],'material'=>$v['material']))->data(array('outsum'=>($shuliang['outsum']+$v['amount'])))->save();
-				     }
-				}
-			}
-			
-	    }
-		
-		// 物资采购申请
-	    if ($row['req_type'] == P::REQ_TYPE_GOODS_PURCHASE) {
-			$dstdata = M()->table('__' . strtoupper($row['req_table']) . '__')->where('id='.$row['req_id'])->find();
-			$matelist = M('material_purchase')->where(array('batch_id'=>$dstdata['id']))->select();
-			foreach($matelist as $v){
-				$at = M('material_purchase')->data(array('audit_status'=>$dst_status))->where(array('id'=>$v['id']))->save();
-				if($v['op_id'] && $dst_status == P::AUDIT_STATUS_PASS){
-					 M('op_material')->where(array('op_id'=>$v['op_id'],'material'=>$v['material']))->setInc('purchasesum', $v['amount']);
-				}
-			}
-			
-	    }
-		
-		// 预算审批
-	    if ($row['req_type'] == P::REQ_TYPE_BUDGET) {
-			$dstdata = M()->table('__' . strtoupper($row['req_table']) . '__')->where('id='.$row['req_id'])->find();
-			
-			//在预算前所建立的物资都无效，需要在预算审批之后调度的才有效
-			M('op_cost')->where(array('op_id'=>$dstdata['op_id'],'cost_type'=>4))->delete();
-			M('op_material')->where(array('op_id'=>$dstdata['op_id']))->delete();
-			
-			$matelist = M('op_costacc')->where(array('op_id'=>$dstdata['op_id'],'status'=>1,'type'=>1))->select();
-			foreach($matelist as $v){
-				
-				$wuzi = M('material')->field('id')->where(array('material'=>$v['title']))->find();
-				$matedata = array();
-				$matedata['op_id'] = $dstdata['op_id'];
-				$matedata['material'] = $v['title'];
-				$matedata['remarks']  = $v['remark'];
-				$matedata['material_id']  = $wuzi['id'];
-				$mateid = M('op_material')->add($matedata);
-				
-				$matecost = array();
-				$matecost['op_id'] = $dstdata['op_id'];
-				$matecost['item']  = '物资费';
-				$matecost['cost']  = $v['unitcost'];
-				$matecost['amount']  = $v['amount'];
-				$matecost['total']  = $v['total'];
-				$matecost['cost_type']  = 4;
-				$matecost['remark']  = $v['title'];
-				$matecost['relevant_id']  = $wuzi['id'];
-				$matecost['link_id']  = $mateid;
-				M('op_cost')->add($matecost);
-				
-			}
-			
-			$record = array();
-			$record['op_id']   = $dstdata['op_id'];
-			$record['optype']  = 10;
-			if($dst_status == P::AUDIT_STATUS_PASS){
-				 $record['explain'] = '预算审核通过'; 
-			}else{
-				 $record['explain'] = '预算审核未通过'; 
-			}
-			op_record($record);
-			
-	    }
-	    
-	    // 价格申请处理
-	    if ($row['req_type'] == P::REQ_TYPE_PRICE  && $dst_status == P::AUDIT_STATUS_PASS) {
-	        $price = intval($audit_param);
-	        $srcdata = M()->table('__' . strtoupper($row['req_table']) . '__')
-	                     ->where('id='.$row['req_id'])->find(); 
-	        
-	        $data['srcdata'] = "op_cost.cost=[" .$srcdata['cost'] ."]  op_cost.total=[" . $srcdata['total'] ."]";
-	        $data['dstdata'] = "op_cost.cost=[" .$price ."]  op_cost.total=[" . $price*$srcdata['amount'] ."]";
-	        
-	        M('op_cost')->where('id=' . $row['req_id'])
-	                    ->setField(array('cost'=>$price, 'total'=>$price*$srcdata['amount']));
-	    }
-		
-		
-		//项目审核通知
-		if ($row['req_type'] == P::REQ_TYPE_PROJECT_NEW){
-			 
-			 $pro = M('op')->find($row['req_id']);
-			 $record = array();
-			 $record['op_id']   = $pro['op_id'];
-			 $record['optype']  = 9;
-			 if($dst_status == P::AUDIT_STATUS_PASS){
-				  $record['explain'] = '项目审核通过'; 
-			 }else{
-				  $record['explain'] = '项目审核未通过'; 
-			 }
-			 op_record($record);
-			 
-	    }
-		
-		
-		
-		// 回款审核
-	    if ($row['req_type'] == P::REQ_TYPE_HUIKUAN) {
-			$dstdata = M()->table('__' . strtoupper($row['req_table']) . '__')->where('id='.$row['req_id'])->find();
-			
-			$record = array();
-			$record['op_id']   = $dstdata['op_id'];
-			$record['optype']  = 10;
-			if($dst_status == P::AUDIT_STATUS_PASS){
-				
-				
-				
-				//回款计划处理
-				if($dstdata['payid']){
-					$paydata	= array();
-					$pay		= M('contract_pay')->find($dstdata['payid']);
-					$pay_amount = $dstdata['huikuan']+$pay['pay_amount'];
-					if($pay_amount >= $pay['amount']){
-						$paydata['status']	= 2;	
-					}else{
-						$paydata['status']	= 1;		
-					}
-					$paydata['pay_amount']	= $pay_amount;
-					$paydata['pay_time']		= $dstdata['huikuan_time'];
-					
-					M('contract_pay')->where(array('id'=>$dstdata['payid']))->data($paydata)->save();
-					
-					//计入结算
-					$huikuan = M('op_huikuan')->where(array('cid'=>$pay['cid'],'audit_status'=>1))->sum('huikuan');
-					M('contract')->data(array('payment'=>$huikuan))->where(array('id'=>$pay['cid']))->save();
-				}
-				
-				
-				$record['explain'] = '预算审核通过'; 
-			}else{
-				$record['explain'] = '预算审核未通过'; 
-			}
-			op_record($record);
-			
-	    }
-	    
-	    return M('audit_log')->where('id='.$id)->save($data);    
-	}
+    protected final function do_audit($audit_id, $audit_reason = '', $dst_status = P::AUDIT_STATUS_PASS, $audit_param = '')
+    {
+        $id   = intval($audit_id);
+        $row  = M('audit_log')->find($id);
+
+        if (!$row) {
+            $this->msg = "非法操作";
+            $this->display('audit_ok');
+            die();
+        }
+
+        if ($row['dst_status'] != P::AUDIT_STATUS_NOT_AUDIT ) {
+            // return P::ERROR;
+        }
+        $data = array();
+
+        $data['audit_uid']   = session('userid');
+        $data['audit_uname'] = session('nickname');
+        $data['audit_reason']= $audit_reason;
+        $data['audit_time']  = time();
+        $data['dst_status']  = $dst_status;
+        $data['audit_param'] = $audit_param;
+
+        M()->table('__' . strtoupper($row['req_table']) . '__')
+            ->where('id='.$row['req_id'])->setField('audit_status', $dst_status);
+
+        // 物资入库处理
+        if ($row['req_type'] == P::REQ_TYPE_GOODS_IN  && $dst_status == P::AUDIT_STATUS_PASS) {
+
+            $dstdata = M()->table('__' . strtoupper($row['req_table']) . '__')->where('id='.$row['req_id'])->find();
+            $matelist = M('material_into')->where(array('batch_id'=>$dstdata['id']))->select();
+            foreach($matelist as $v){
+                $at = M('material_into')->data(array('audit_status'=>$dst_status))->where(array('id'=>$v['id']))->save();
+                if($at){
+                    if($dst_status == P::AUDIT_STATUS_PASS ){
+                        $wzinfo = M('material')->find($v['material_id']);
+                        if($v['type']){
+                            M('material')->where(array('id'=>$v['material_id']))->setField(array('stock'=>$wzinfo['stock']+$v['amount']));
+                        }else{
+                            M('material')->where(array('id'=>$v['material_id']))->setField(array('price'=>$v['unit_price'], 'stock'=>$wzinfo['stock']+$v['amount']));
+                        }
+
+                        if($v['op_id']){
+                            $shuliang = M('op_material')->where(array('op_id'=>$v['op_id'],'material'=>$v['material']))->find();
+                            M('op_material')->where(array('op_id'=>$v['op_id'],'material'=>$v['material']))->data(array('returnsum'=>($shuliang['returnsum']+$v['amount'])))->save();
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+        // 物资出库处理
+        if ($row['req_type'] == P::REQ_TYPE_GOODS_OUT) {
+            $dstdata = M()->table('__' . strtoupper($row['req_table']) . '__')->where('id='.$row['req_id'])->find();
+            $matelist = M('material_out')->where(array('batch_id'=>$dstdata['id']))->select();
+            foreach($matelist as $v){
+                $at = M('material_out')->data(array('audit_status'=>$dst_status))->where(array('id'=>$v['id']))->save();
+                if($at){
+                    if($dst_status == P::AUDIT_STATUS_PASS ){
+                        M('material')->where(array('id'=>$v['material_id']))->setDec('stock', $v['amount']);
+                    }
+                    if($v['op_id'] && $dst_status == P::AUDIT_STATUS_PASS){
+                        $shuliang = M('op_material')->where(array('op_id'=>$v['op_id'],'material'=>$v['material']))->find();
+                        M('op_material')->where(array('op_id'=>$v['op_id'],'material'=>$v['material']))->data(array('outsum'=>($shuliang['outsum']+$v['amount'])))->save();
+                    }
+                }
+            }
+
+        }
+
+        // 物资采购申请
+        if ($row['req_type'] == P::REQ_TYPE_GOODS_PURCHASE) {
+            $dstdata = M()->table('__' . strtoupper($row['req_table']) . '__')->where('id='.$row['req_id'])->find();
+            $matelist = M('material_purchase')->where(array('batch_id'=>$dstdata['id']))->select();
+            foreach($matelist as $v){
+                $at = M('material_purchase')->data(array('audit_status'=>$dst_status))->where(array('id'=>$v['id']))->save();
+                if($v['op_id'] && $dst_status == P::AUDIT_STATUS_PASS){
+                    M('op_material')->where(array('op_id'=>$v['op_id'],'material'=>$v['material']))->setInc('purchasesum', $v['amount']);
+                }
+            }
+
+        }
+
+        // 预算审批
+        if ($row['req_type'] == P::REQ_TYPE_BUDGET) {
+            $dstdata = M()->table('__' . strtoupper($row['req_table']) . '__')->where('id='.$row['req_id'])->find();
+
+            //在预算前所建立的物资都无效，需要在预算审批之后调度的才有效
+            M('op_cost')->where(array('op_id'=>$dstdata['op_id'],'cost_type'=>4))->delete();
+            M('op_material')->where(array('op_id'=>$dstdata['op_id']))->delete();
+
+            $matelist = M('op_costacc')->where(array('op_id'=>$dstdata['op_id'],'status'=>1,'type'=>1))->select();
+            foreach($matelist as $v){
+
+                $wuzi = M('material')->field('id')->where(array('material'=>$v['title']))->find();
+                $matedata = array();
+                $matedata['op_id'] = $dstdata['op_id'];
+                $matedata['material'] = $v['title'];
+                $matedata['remarks']  = $v['remark'];
+                $matedata['material_id']  = $wuzi['id'];
+                $mateid = M('op_material')->add($matedata);
+
+                $matecost = array();
+                $matecost['op_id'] = $dstdata['op_id'];
+                $matecost['item']  = '物资费';
+                $matecost['cost']  = $v['unitcost'];
+                $matecost['amount']  = $v['amount'];
+                $matecost['total']  = $v['total'];
+                $matecost['cost_type']  = 4;
+                $matecost['remark']  = $v['title'];
+                $matecost['relevant_id']  = $wuzi['id'];
+                $matecost['link_id']  = $mateid;
+                M('op_cost')->add($matecost);
+
+            }
+
+            $record = array();
+            $record['op_id']   = $dstdata['op_id'];
+            $record['optype']  = 10;
+            if($dst_status == P::AUDIT_STATUS_PASS){
+                $record['explain'] = '预算审核通过';
+            }else{
+                $record['explain'] = '预算审核未通过';
+            }
+            op_record($record);
+
+        }
+
+        // 价格申请处理
+        if ($row['req_type'] == P::REQ_TYPE_PRICE  && $dst_status == P::AUDIT_STATUS_PASS) {
+            $price = intval($audit_param);
+            $srcdata = M()->table('__' . strtoupper($row['req_table']) . '__')
+                ->where('id='.$row['req_id'])->find();
+
+            $data['srcdata'] = "op_cost.cost=[" .$srcdata['cost'] ."]  op_cost.total=[" . $srcdata['total'] ."]";
+            $data['dstdata'] = "op_cost.cost=[" .$price ."]  op_cost.total=[" . $price*$srcdata['amount'] ."]";
+
+            M('op_cost')->where('id=' . $row['req_id'])
+                ->setField(array('cost'=>$price, 'total'=>$price*$srcdata['amount']));
+        }
+
+
+        //项目审核通知
+        if ($row['req_type'] == P::REQ_TYPE_PROJECT_NEW){
+
+            $pro = M('op')->find($row['req_id']);
+            $record = array();
+            $record['op_id']   = $pro['op_id'];
+            $record['optype']  = 9;
+            if($dst_status == P::AUDIT_STATUS_PASS){
+                $record['explain'] = '项目审核通过';
+            }else{
+                $record['explain'] = '项目审核未通过';
+            }
+            op_record($record);
+
+        }
+
+        //结算审核
+        if ($row['req_type'] == P::REQ_TYPE_SETTLEMENT) {
+            $dstdata = M()->table('__' . strtoupper($row['req_table']) . '__')->where('id='.$row['req_id'])->find();
+
+            $record = array();
+            $record['op_id']   = $dstdata['op_id'];
+            $record['optype']  = 10;
+            if($dst_status == P::AUDIT_STATUS_PASS){
+                $record['explain'] = '结算审核通过';
+            }else{
+                $record['explain'] = '结算审核未通过';
+            }
+            op_record($record);
+
+        }
+
+        // 回款审核
+        if ($row['req_type'] == P::REQ_TYPE_HUIKUAN) {
+            $dstdata = M()->table('__' . strtoupper($row['req_table']) . '__')->where('id='.$row['req_id'])->find();
+
+            $record = array();
+            $record['op_id']   = $dstdata['op_id'];
+            $record['optype']  = 10;
+            if($dst_status == P::AUDIT_STATUS_PASS){
+
+
+
+                //回款计划处理
+                if($dstdata['payid']){
+                    $paydata	= array();
+                    $pay		= M('contract_pay')->find($dstdata['payid']);
+                    $pay_amount = $dstdata['huikuan']+$pay['pay_amount'];
+                    if($pay_amount >= $pay['amount']){
+                        $paydata['status']	= 2;
+                    }else{
+                        $paydata['status']	= 1;
+                    }
+                    $paydata['pay_amount']	= $pay_amount;
+                    $paydata['pay_time']		= $dstdata['huikuan_time'];
+
+                    M('contract_pay')->where(array('id'=>$dstdata['payid']))->data($paydata)->save();
+
+                    //计入结算
+                    $huikuan = M('op_huikuan')->where(array('cid'=>$pay['cid'],'audit_status'=>1))->sum('huikuan');
+                    M('contract')->data(array('payment'=>$huikuan))->where(array('id'=>$pay['cid']))->save();
+                }
+
+
+                $record['explain'] = '回款审核通过';
+            }else{
+                $record['explain'] = '回款审核未通过';
+            }
+            op_record($record);
+
+        }
+
+        return M('audit_log')->where('id='.$id)->save($data);
+    }
 
 	/**
      * ajaxPageHtml 分页
