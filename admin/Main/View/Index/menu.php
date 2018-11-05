9d151cd4bdbe54cd5eec9feb01c615e3b2c26a89
unknown <1021056947@qq.com>
2018-11-05 18:40:33 +0800

    aa

--- admin/Main/View/Index/menu.php
+++ admin/Main/View/Index/menu.php
@@ -440,13 +440,16 @@
                             </a>
                             <ul class="treeview-menu">
 
-                                <li class="treeview {:on('Finance/jiekuan')}">
+                                <li class="treeview {:on('Finance/jiekuan')} {:on('Finance/sign')}">
                                     <if condition="rolemenu(array('Finance/jiekuan'))">
                                         <a href=""><i class="fa  fa-check-circle"></i> 财务审批</a>
                                     </if>
                                     <ul class="treeview-menu">
                                         <if condition="rolemenu(array('Finance/jiekuan'))">
                                             <li class="{:on('Finance/jiekuan')}"><a href="{:U('Finance/jiekuan')}"><i class="fa fa-angle-right"></i> 团内支出借款</a></li>
+                                        </if>
+                                        <if condition="rolemenu(array('Finance/sign'))">
+                                            <li class="{:on('Finance/sign')}"><a href="{:U('Finance/sign')}"><i class="fa fa-angle-right"></i> 个人签字管理</a></li>
                                             <!--<li><a href="javascript:;" onclick="art_show_msg('加班开发中,稍后呈现......',5)"><i class="fa fa-angle-right"></i>团内支出借款</a></li>-->
                                         </if>
                                     </ul>
