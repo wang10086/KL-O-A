a38a9e06fe523ff33769b55a50da1a478fc6178a
root <1021056947@qq.com>
2018-11-29 18:48:24 +0800

    no message

--- admin/Main/View/Manage/Manage_year.php
+++ admin/Main/View/Manage/Manage_year.php
@@ -23,7 +23,7 @@
 
                 <div class="box box-warning">
                     <div class="box-header">
-                        <h3 class="box-title">年度经营报表</h3>
+                        <h3 class="box-title">年度预算报表</h3>
                     </div><!-- /.box-header -->
                     <div class="box-body">
 
@@ -193,6 +193,182 @@
                         </table><br><br>
 
                     </div><!-- /.box-body -->
+
+                    <div class="box box-warning" style="border-top-color:#ddd;">
+                        <div class="box-header">
+                            <h3 class="box-title">年度经营报表</h3>
+                        </div><!-- /.box-header -->
+                        <div class="box-body">
+
+                            <table class="table table-bordered dataTable fontmini" id="tablecenter">
+                                <tr role="row" class="orders" style="text-align:center;" >
+                                    <th style="width:10em;" rowspan="2"><b>项目</b></th>
+                                    <th style="width:10em;" rowspan="2"><b>公司</b></th>
+                                    <th style="width:10em;" colspan="2"><b>京区业务中心</b></th>
+                                    <th style="width:10em;" colspan="2"><b>京外业务中心</b></th>
+                                    <th style="width:10em;" colspan="2"><b>南京项目部</b></th>
+                                    <th style="width:10em;" colspan="2"><b>武汉项目部</b></th>
+                                    <th style="width:10em;" colspan="2"><b>沈阳项目部</b></th>
+                                    <th style="width:10em;" colspan="2"><b>长春项目部</b></th>
+                                    <th style="width:10em;" colspan="2"><b>市场部</b></th>
+                                </tr>
+                                <tr role="row" class="orders" style="text-align:center;">
+                                    <?php for($i=0;$i<7;$i++){?>
+                                        <td>数额</td>
+                                        <td>占比</td>
+                                    <?php } ?>
+                                </tr>
+
+                                <tr role="row" class="orders" style="text-align:center;">
+                                    <td>员工人数</td>
+                                    <td>{$number[0]['employees_sum']}</td>
+                                    <td>{$number[1]['employees_sum']}</td>
+                                    <td>{$number[1]['proportion']}</td>
+                                    <td>{$number[2]['employees_sum']}</td>
+                                    <td>{$number[2]['proportion']}</td>
+                                    <td>{$number[3]['employees_sum']}</td>
+                                    <td>{$number[3]['proportion']}</td>
+                                    <td>{$number[4]['employees_sum']}</td>
+                                    <td>{$number[4]['proportion']}</td>
+                                    <td>{$number[5]['employees_sum']}</td>
+                                    <td>{$number[5]['proportion']}</td>
+                                    <td>{$number[6]['employees_sum']}</td>
+                                    <td>{$number[6]['proportion']}</td>
+                                    <td>{$number[7]['employees_sum']}</td>
+                                    <td>{$number[7]['proportion']}</td>
+                                </tr>
+                                <tr role="row" class="orders" style="text-align:center;">
+                                    <td>营业收入</td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr role="row" class="orders" style="text-align:center;">
+                                    <td>营业毛利</td>
+                                    <td><?php echo $n['employees_sum'];?></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr role="row" class="orders" style="text-align:center;">
+                                    <td>营业毛利率(%)</td>
+                                    <td><?php echo $n['employees_sum'];?></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr role="row" class="orders" style="text-align:center;">
+                                    <td>人力资源成本</td>
+                                    <td><?php echo $n['employees_sum'];?></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr role="row" class="orders" style="text-align:center;">
+                                    <td>其他费用</td>
+                                    <td><?php echo $n['employees_sum'];?></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr role="row" class="orders" style="text-align:center;">
+                                    <td>利润总额</td>
+                                    <td><?php echo $n['employees_sum'];?></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr role="row" class="orders" style="text-align:center;">
+                                    <td>人事费用率</td>
+                                    <td><?php echo $n['employees_sum'];?></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>1</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </table><br><br>
+
+                        </div><!-- /.box-body -->
+                    </div><!-- /.box -->
+
+
                 </div><!-- /.box -->
 
             </div><!--/.col (right) -->
