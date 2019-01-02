5eb5903d9c7fe8f42e7774c34b2c72fe4093beb9
root <1021056947@qq.com>
2019-01-02 11:39:30 +0800

    no message

--- admin/Main/View/Salary/salary_excel_list.php
+++ admin/Main/View/Salary/salary_excel_list.php
@@ -210,9 +210,9 @@
                     </div><!-- /.box-body -->
 
                     <div style="margin-top:2em;text-align:center;" id="shr_qianzi"><br><br><br><br>
-<!--                        --><?php //if($status==1 && $userid== 77){?>
+                        <?php if($status==1 && $userid== 77){?>
                             <a  class="btn btn-info salary_excel1_submit" style="width:10em;margin-top:2em;" onclick="show_qianzi(0)">提交审核</a>
-<!--                        --><?php //}?>
+                        <?php }?>
                         <?php if($status==2 && $userid == 55){?>
                             <a  class="btn btn-info salary_excel1_submit1" style="width:10em;margin-top:2em;" onclick="show_qianzi(1)">提交批准</a>
                             <a  class="btn btn-info salary_excel1_submit2" style="width:10em;margin-top:2em;" onclick="show_qianzi(2)">驳回</a>
