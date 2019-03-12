2a321c86840617b4cec6d3b6d6269e10f3abe6ec
root <10086@qq.com>
2019-03-12 18:27:06 +0800

    aa

--- admin/Main/View/Index/satisfaction_detail.php
+++ admin/Main/View/Index/satisfaction_detail.php
@@ -23,6 +23,7 @@
             <?php if ($list['BB']){ ?> <div class="form-group box-float-4" style="padding-left:0;">{$list.bb}：{$list.BB}&nbsp;分</div> <?php } ?>
             <?php if ($list['CC']){ ?> <div class="form-group box-float-4" style="padding-left:0;">{$list.cc}：{$list.CC}&nbsp;分</div> <?php } ?>
             <?php if ($list['DD']){ ?> <div class="form-group box-float-4" style="padding-left:0;">{$list.dd}：{$list.DD}&nbsp;分</div> <?php } ?>
+            <?php if ($list['EE']){ ?> <div class="form-group box-float-4" style="padding-left:0;">{$list.ee}：{$list.EE}&nbsp;分</div> <?php } ?>
 
              <div class="form-group box-float-12" style="padding-left:0;">意见建议：{$list.content}</div>
         </div>
