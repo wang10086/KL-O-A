392dff83c5cbe92e82933bbb5eded8cd1b3ae0f6
root <10086@qq.com>
2019-03-30 17:58:37 +0800

    aa

--- admin/Common/Common/function.php
+++ admin/Common/Common/function.php
@@ -1124,6 +1124,8 @@ function unique_arr($array,$field='name'){
      */
     function QR_code($data,$name = 0,$size = 30){
 
+        return "aaatest";
+
         ulib('phpqrcode.phpqrcode');
 
         $QRcode = new \QRcode();
