c85562b922c10ac6273d444cfc7e635e3776be75
root <10086@qq.com>
2019-04-24 20:07:24 +0800

    aa

--- admin/Main/Model/ContractModel.class.php
+++ admin/Main/Model/ContractModel.class.php
@@ -17,11 +17,14 @@ class ContractModel extends Model{
      * @return mixed
      */
     public function get_budget_list($sale_user_id,$begintime,$endtime){
+        $dj_opids                           = array_filter(M('op')->group('dijie_opid')->getField('dijie_opid',true));
         $where                              = array(); //当月预算审核通过的项目
         $where['l.req_type']                = P::REQ_TYPE_BUDGET; //预算申请
         $where['b.audit_status']            = 1; //审批通过
         $where['l.audit_time']              = array('between',"$begintime,$endtime");
-        $where['create_user']               = $sale_user_id;
+        $where['o.create_user']             = $sale_user_id;
+        $where['b.op_id']                   = array('not in',$dj_opids);
+        $where['c.dep_time']                = array('lt',$endtime-7*24*3600);
         $field                              = 'l.audit_uid,audit_uname,audit_time,c.dep_time,c.ret_time,o.*';
         $lists                              = M()->table('__OP_BUDGET__ as b')->join('__AUDIT_LOG__ as l on l.req_id=b.id','left')->join('__OP__ as o on o.op_id = b.op_id','left')->join('left join __OP_TEAM_CONFIRM__ as c on c.op_id=b.op_id')->field($field)->where($where)->select();
         return $lists;
