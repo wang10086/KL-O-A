407c094023fa6122c672cd974f10b0102ebf6583
root <10086@qq.com>
2019-04-24 20:30:06 +0800

    aa

--- admin/Main/Model/ContractModel.class.php
+++ admin/Main/Model/ContractModel.class.php
@@ -23,8 +23,8 @@ class ContractModel extends Model{
         $where['b.audit_status']            = 1; //审批通过
         $where['l.audit_time']              = array('between',"$begintime,$endtime");
         $where['o.create_user']             = $sale_user_id;
-        $where['b.op_id']                   = array('not in',$dj_opids);
-        $where['c.dep_time']                = array('lt',$endtime-7*24*3600);
+        $where['b.op_id']                   = array('not in',$dj_opids); //排除地接团
+        $where['c.dep_time']                = array('lt',$endtime-7*24*3600); //5个工作日前
         $field                              = 'l.audit_uid,audit_uname,audit_time,c.dep_time,c.ret_time,o.*';
         $lists                              = M()->table('__OP_BUDGET__ as b')->join('__AUDIT_LOG__ as l on l.req_id=b.id','left')->join('__OP__ as o on o.op_id = b.op_id','left')->join('left join __OP_TEAM_CONFIRM__ as c on c.op_id=b.op_id')->field($field)->where($where)->select();
         return $lists;
