bf3098ae9412ce842c44cb21e083e9c4da683959
root <10086@qq.com>
2019-07-22 14:30:21 +0800

    aa

--- admin/Common/Common/function.php
+++ admin/Common/Common/function.php
@@ -3090,8 +3090,8 @@ function updatekpi($month,$user){
                             $operate_info   = get_sum_department_operate($department,$year,$monon,'y');     //实际经营信息(年度累计)
                             $jy_lrze        = round($operate_info['lrze'],2);   //经营利润总额
                             $quart          = quarter_month1($monon);
-                            $url            = U('manage/Manage_quarter',array('year'=>$v['year'],'quart'=>$quart));
-
+                            //$url            = U('manage/Manage_quarter',array('year'=>$v['year'],'quart'=>$quart));
+                            $url            = U('Manage/Manage_year',array('year'=>$v['year']));
                             $complete       = $jy_lrze;
                         }
 
