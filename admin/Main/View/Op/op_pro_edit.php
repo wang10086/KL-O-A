8aa0ffef5e716df02a2afab5aae53360cd0adac8
root <10086@qq.com>
2019-10-29 17:17:14 +0800

    aa

--- admin/Main/View/Op/op_pro_edit.php
+++ admin/Main/View/Op/op_pro_edit.php
@@ -95,6 +95,7 @@
 
     <div class="form-group col-md-4" id="dijie_or_sale"></div>
     <?php if ($is_dijie){ ?> <!--内部地接-->
+    <div class="form-group col-md-12">
         <div class="form-group col-md-8">
             <div class="form-group col-md-12" style="margin-left:-15px;">
                 <label>备注：</label><textarea class="form-control"  name="info[context]">{$op.context}</textarea>
@@ -109,6 +110,7 @@
             <label style="width:100%; border-bottom:1px solid #dedede; padding-bottom:10px; font-weight:bold;">内部满意度评分二维码&emsp;<span class="red">(请发送给组团方：{$is_dijie.create_user_name})</span></label>
             <div id="code"></div>
         </div>
+    </div>
     <?php }else{ ?>
         <div class="form-group col-md-12">
             <label>备注：</label><textarea class="form-control"  name="info[context]">{$op.context}</textarea>
