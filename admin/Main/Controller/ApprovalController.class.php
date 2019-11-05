f329bd30136fea7b59539e5c10a7960a797da6e6
root <10086@qq.com>
2019-11-05 19:11:02 +0800

    aa

--- admin/Main/Controller/ApprovalController.class.php
+++ admin/Main/Controller/ApprovalController.class.php
@@ -5,6 +5,7 @@ use Think\Upload;
 use Sys\P;
 ulib('Page');
 use Sys\Page;
+
 class ApprovalController extends BaseController {
     protected $_pagetitle_ = '文件审批';
 
@@ -55,42 +56,39 @@ class ApprovalController extends BaseController {
         }
     }
 
-    public function upload_file ()
+    public function public_upload_file ()
     {
-        $db = M('attachment');
-        $upload = new Upload(C('UPLOAD_FILE_CFG'));
-        $info = $upload->upload();
-        $att = array();
-        $rs = array();
+        $db         = M('approval_files');
+        $upload     = new Upload(C('UPLOAD_FILE_CFG'));
+        $info       = $upload->upload();
+        $att        = array();
+        $rs         = array();
 
         if ($info) {
             foreach ($info as $row) {
                 $rs['rs'] = 'ok';
                 $rs['fileurl'] = $upload->rootPath . $row['savepath'] . $row['savename'];
-                if (in_array( strtolower($row['ext']), array('jpg','jpeg','png','gif','bmp','svg'))) {
+                /*if (in_array( strtolower($row['ext']), array('jpg','jpeg','png','gif','bmp','svg'))) {
                     // $rs['thumb']   = thumb($rs['fileurl'], C('DEFAULT_THUMB_W'), C('DEFAULT_THUMB_H'));
                     $att['isimage'] = 1;
                 } else {
                     $att['isimage'] = 0;
-                }
+                }*/
 
-                $att['filesize']    = $row['size'];
-                $att['fileext']     = $row['ext'];
-                $att['filename']    = $row['name'];
-                $att['filepath']    = $rs['fileurl'];
-                $att['catid']       = I('catid');
+                $att['filesize']    = $row['size'] ? $row['size'] : 0;
+                $att['fileext']     = $row['ext'] ? $row['ext'] : 0;
+                $att['filename']    = $row['name'] ? $row['name'] : 0;
+                $att['filepath']    = $rs['fileurl'] ? $rs['fileurl'] : '00001';
+                $att['fileType']    = I('fileType') ? I('fileType') : 9;
                 $att['userid']      = session('userid')?session('userid'):0;
                 $att['uploadtime']  = time();
                 $att['uploadip']    = get_client_ip();
-                $att['rel_id']      = 0;
                 $att['hashcode']    = $row['md5'];
 
                 $aid = $db->add($att);
                 $rs['aid'] = $aid;
-
                 break;
             }
-
             echo json_encode($rs);
 
         } else {
