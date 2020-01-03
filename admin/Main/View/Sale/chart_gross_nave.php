5abd1ea297c9df9ed089714151e9708c08392d0d
王超 <1021056947@qq.com>
2020-01-03 16:37:09 +0800

    毛利率统计 + 年度条

--- admin/Main/View/Sale/chart_gross_nave.php
+++ admin/Main/View/Sale/chart_gross_nave.php
@@ -2,3 +2,9 @@
     <a href="{:U('Sale/chart_gross')}" class="btn <?php if(ACTION_NAME=='chart_gross'){ echo 'btn-info';}else{ echo 'btn-default';} ?>">公司毛利率</a>
     <a href="{:U('Sale/chart_jd_gross')}" class="btn <?php if(ACTION_NAME=='chart_jd_gross'){ echo 'btn-info';}else{ echo 'btn-default';} ?>">各计调毛利率</a>
 </div>
+
+<div class="btn-group" id="catfont" style="float: right; clear: right;">
+    <?php for ($i = 2018; $i <= date('Y'); $i++){ ?>
+        <a href="<?php echo U('Sale/'.ACTION_NAME , array('year'=>$i)) ?>" class="btn <?php if($year==$i){ echo 'btn-info';}else{ echo 'btn-default';} ?>">{$i}年</a>
+    <?php } ?>
+</div>
