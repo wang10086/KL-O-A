a7aa92cb8f5215374926a24057d0fd6cd642937f
root <10086@qq.com>
2020-01-06 15:27:56 +0800

    aa

--- admin/Main/View/Approval/index.php
+++ admin/Main/View/Approval/index.php
@@ -64,7 +64,7 @@
                                             <td class="taskOptions">
                                                 <?php if ($row['status'] == 0){ ?>
                                                     <a href="{:U('Approval/file_upload',array('id'=>$row['id']))}" title="编辑" class="btn btn-info btn-smsm"><i class="fa fa-pencil"></i></a>
-                                                <?php }elseif($row['status'] == 3){ ?>
+                                                <?php }elseif($row['status'] == 3 && in_array(cookie('userid'),array($row['create_user'],1))){ ?>
                                                     <a href="{:U('Approval/file_re_upload',array('id'=>$row['id']))}" title="编辑" class="btn btn-primary btn-smsm"><i class="fa fa-pencil"></i></a>
                                                 <?php } ?>
                                             </td>
