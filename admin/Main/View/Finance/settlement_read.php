bab43824f03ae505791f89ed148ac313e2414993
root <10086@qq.com>
2020-01-07 17:36:47 +0800

    finance    settlement

--- admin/Main/View/Finance/settlement_read.php
+++ admin/Main/View/Finance/settlement_read.php
@@ -1,4 +1,4 @@
-<?php if($is_zutuan == 1 && $dijie_shouru == 0){ ?>
+<?php if($is_zutuan == 1 && $dijie_shouru == 0 && $op['kind'] != 87){ ?>  <!--87=>排除单进院所,单进院所暂时未生成地接团-->
     <div class="content" style="margin-left:15px;">地接部门尚未做该地接项目的结算！</div>
 <?php }else{ ?>
     <div class="content" style="padding-top:0px;">
