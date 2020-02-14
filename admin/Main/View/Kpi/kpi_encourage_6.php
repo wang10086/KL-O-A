506df3f72ced35706af77104b4cb6bad57dcc92d
王超 <1021056947@qq.com>
2020-02-14 12:07:38 +0800

    aa

--- admin/Main/View/Kpi/kpi_encourage_6.php
+++ admin/Main/View/Kpi/kpi_encourage_6.php
@@ -20,7 +20,7 @@
                         <th class="taskOptions">合伙人合计毛利增量奖金⑧</th>
                         <th class="taskOptions">本季度奖金合计⑨</th>
                     </tr>-->
-                    <tr role="row" class="orders" >
+                    <!--<tr role="row" class="orders" >
                         <th class="taskOptions">本季度科学快车毛利</th>
                         <th class="taskOptions">上年同周期科学快车毛利</th>
                         <th class="taskOptions">本季度科学快车毛利增量</th>
@@ -41,6 +41,37 @@
                         <td class="taskOptions">{$encourage_data.royalty10}</td>
                         <td class="taskOptions">{$encourage_data.sum_should_royalty}</td>
                         <td class="taskOptions">{$encourage_data.quarter_should_royalty}</td>
+                    </tr>-->
+                </table>
+                <div class="form-group"></div>
+
+                <table class="table table-bordered dataTable fontmini" id="tablelist" style="margin-top:10px;">
+                    <tr role="row" class="orders" >
+                        <th class="taskOptions">年度</th>
+                        <th class="taskOptions">本季度科学快车毛利</th>
+                        <!--<th class="taskOptions">上年同周期科学快车毛利</th>-->
+                        <th class="taskOptions">本季度科学快车毛利增量</th>
+                        <th class="taskOptions">科学快车毛利增量奖金</th>
+                        <th class="taskOptions">本季度合伙人合计毛利</th>
+                        <!--<th class="taskOptions">上年同周期合伙人合计毛利</th>-->
+                        <th class="taskOptions">本季度合伙人合计毛利增量</th>
+                        <th class="taskOptions">合伙人合计毛利增量奖金</th>
+                        <th class="taskOptions">本季度奖金合计</th>
+                    </tr>
+                    <tr>
+                        <td class="taskOptions">{$year-1}年</td>
+                        <td class="taskOptions">{$encourage_data.lastYearProfit}</td>
+                        <td class="taskOptions" rowspan="2">{$encourage_data.sum_target}</td>
+                        <td class="taskOptions" rowspan="2">{$encourage_data.thisYearSumProfit}</td>
+                        <td class="taskOptions">{$encourage_data.royalty5}</td>
+                        <td class="taskOptions" rowspan="2">{$encourage_data.royalty10}</td>
+                        <td class="taskOptions" rowspan="2">{$encourage_data.sum_should_royalty}</td>
+                        <td class="taskOptions" rowspan="2">{$encourage_data.quarter_should_royalty}</td>
+                    </tr>
+                    <tr>
+                        <td class="taskOptions">{$year}年</td>
+                        <td class="taskOptions">{$encourage_data.thisYearProfit}</td>
+                        <td class="taskOptions">{$encourage_data.thisYearSumProfit}</td>
                     </tr>
                 </table>
 
