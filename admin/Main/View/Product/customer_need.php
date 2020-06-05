d9518c2896e9f2946cc5e0f88a13e1342256600a
王超 <1021056947@qq.com>
2020-06-05 10:58:45 +0800

    客户需求详情   box-title code

--- admin/Main/View/Product/customer_need.php
+++ admin/Main/View/Product/customer_need.php
@@ -100,7 +100,7 @@
                                 </div><!-- /.box-body -->
                             </div><!-- /.box -->
 
-                            <?php if (!$budget_list && in_array(cookie('userid'),array(1,11,$list['create_user'],$list['line_blame_uid']))){ ?>
+                            <?php if (!$budget_list && in_array(cookie('userid'),array(11,$list['create_user'],$list['line_blame_uid']))){ ?>
                                 <?php if ($list['kind'] == 60){ ?> <!--60=>科学课程-->
                                     <include file="customer_need_60_edit" />
                                 <?php }elseif ($list['kind'] == 82){ ?> <!--82=>科学博物园-->
